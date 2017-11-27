8b57a29a1490bbfc7566fba2c88f8cdc49ca2bfe
wesleydeveloper <wesley.developer@gmail.com>
2017-11-27 01:51:34 -0200

    Refactory;

--- kidfriendly-service/kidfriendly-domain/src/main/java/com/holictechnology/kidfriendly/domain/dto/filter/AbstractFilterDto.java
+++ kidfriendly-service/kidfriendly-domain/src/main/java/com/holictechnology/kidfriendly/domain/dto/filter/AbstractFilterDto.java
@@ -8,7 +8,7 @@ import javax.ws.rs.BeanParam;
 import com.holictechnology.kidfriendly.domain.dto.paginator.PaginatorDto;
 
 
-public class AbstractFilterDto implements Serializable {
+public abstract class AbstractFilterDto implements Serializable {
 
     private static final long serialVersionUID = -787193217721323332L;
 
